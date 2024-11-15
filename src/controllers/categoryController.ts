17ef4836dcecbc497f2dff8d5ed72541a39a1015
ishanrana363 <ishanrana363@gmail.com>
2024-11-15 18:22:33 +0600

    category update

--- src/controllers/categoryController.ts
+++ src/controllers/categoryController.ts
@@ -4,18 +4,46 @@ import express, { Request, Response } from 'express';
 
 export const createCategory = async (req: any, res: any) => {
     try {
-        let {name,img} = req.body;
-        let category = new categoryModel({name, img});
+        let { name, img } = req.body;
+        let category = new categoryModel({ name, img });
         await category.save();
         return res.status(201).json({
-            status:"success",
-            message:"Category created successfully",
-            data:category
+            status: "success",
+            message: "Category created successfully",
+            data: category
         })
     } catch (error: any) {
         return res.status(500).json({
-            status:"fail",
-            message:error.message
+            status: "fail",
+            message: error.message
         });
+    }
+};
+
+export const updateCategory = async (req: any, res: any) => {
+    const { id } = req.params;
+    const { name, img } = req.body;
+    let filter = {
+        _id: id
+    }
+
+    try {
+        const updatedCategory = await categoryModel.findByIdAndUpdate(
+            filter,
+            { name, img },
+            { new: true } // Return the updated document
+        );
+
+        if (!updatedCategory) {
+            return res.status(404).json({ status: "fail", message: "Category not found" });
+        }
+
+        res.status(200).json({
+            status: "success",
+            message: "Category updated successfully",
+            data: updatedCategory,
+        });
+    } catch (error: any) {
+        res.status(500).json({ status: "fail", message: error.message });
     }
 };
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { createUser, loginUser, userProfile } from '../controllers/userController';  // Ensure path is correct
 import { authMiddleware } from '../middleware/authMiddleware';
-import {createCategory} from "../controllers/categoryController"
+import {createCategory, updateCategory} from "../controllers/categoryController"
 
 const router = express.Router();
 
@@ -10,11 +10,10 @@ router.post('/register', createUser);
 
 // Post route for user login
 router.post('/login', loginUser);
-
 router.get(`/user-profile`,authMiddleware,userProfile);
 
 // category related api
-
 router.post('/category',authMiddleware, createCategory); // create a new category
+router.put("/category-update/:id", authMiddleware, updateCategory);
 
 export default router;
